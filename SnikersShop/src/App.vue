f0048f20b3559d3261d4f8c6b65525fb7788abe3
K1R1LL117 <n17k17@yandex.ru>
2024-07-24 15:34:59 +0300

    llllll

--- SnikersShop/src/App.vue
+++ SnikersShop/src/App.vue
@@ -3,5 +3,8 @@ import MyHello from './components/MyHello.vue'
 </script>
 
 <template>
+  <MyHello title="Кросовки Puma" price="9500" />
+  <MyHello />
+  <MyHello />
   <MyHello />
 </template>
--- SnikersShop/src/components/MyHello.vue
+++ SnikersShop/src/components/MyHello.vue
@@ -1,19 +1,21 @@
-<script>
-export default {
-  data() {
-    return {
-      count: 0
-    }
-  },
-  methods: {
-    increment() {
-      this.count++
-    }
-  }
-}
+<script setup>
+defineProps({
+  title: String,
+  price: Number
+})
 </script>
 
 <template>
-  <h1>{{ count }}</h1>
-  <button @click="increment">+</button>
+  <div class="product">
+    <h2>Товар: {{ title }}</h2>
+    <h3>Цена: {{ price }}</h3>
+  </div>
 </template>
+
+<style scoped>
+.product {
+  border: 1px solid black;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+</style>
