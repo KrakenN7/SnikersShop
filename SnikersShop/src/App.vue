27a0b1cd102c9fb221f74b2c819bfdd6ac11f733
K1R1LL117 <n17k17@yandex.ru>
2024-07-24 16:22:30 +0300

    ///

--- SnikersShop/src/App.vue
+++ SnikersShop/src/App.vue
@@ -3,7 +3,7 @@ import MyHello from './components/MyHello.vue'
 </script>
 
 <template>
-  <div class="mb-40">
+  <div class="mb-5">
     <MyHello title="Кросовки puma" :price="19500" />
     <MyHello title="Кросовки puma" :price="19500" />
   </div>
