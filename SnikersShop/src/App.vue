75db4fade537af4cfe0467e26854b2e03133b20a
K1R1LL117 <n17k17@yandex.ru>
2024-07-25 13:04:34 +0300

    Add Card, CardList components

--- SnikersShop/src/App.vue
+++ SnikersShop/src/App.vue
@@ -1,10 +1,16 @@
 <script setup>
-import MyHello from './components/MyHello.vue'
+import Header from './components/Header.vue'
+import CardList from './components/CardList.vue'
 </script>
 
 <template>
-  <div class="mb-5">
-    <MyHello title="Кросовки puma" :price="19500" />
-    <MyHello title="Кросовки puma" :price="19500" />
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-10">
+    <Header />
+
+    <div class="p-10">
+      <h2 class="text-3xl font-bold mb-8">Все кросовки</h2>
+
+      <CardList />
+    </div>
   </div>
 </template>
---
+++ SnikersShop/src/components/CardList.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Card from './Card.vue'
+
+const onClickAdd = () => {
+  alert('Добавить')
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      title="Мужские кросовки Nike Blazer Mid Suede"
+      imageUrl="/sneakers/sneakers-1.jpg"
+      :price="5000"
+      :isA-dded="false"
+      :onClickAdd="onClickAdd"
+    />
+    <!-- <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card /> -->
+  </div>
+</template>
